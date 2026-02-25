c4ddc8b43da8c36395cf0aa9e511c09877e519b1
conniehynd <constance.hyndman@postgrad.manchester.ac.uk>
2026-02-25 16:19:55 +0000

    right

--- A1.R
+++ A1.R
@@ -1,9 +1,13 @@
-setwd("~/Desktop/Spatial Ecology")
+setwd("~/Desktop/Spatial Ecology/Week_1")
 
 #Install packages
-install.packages(c("terra","sf","mapview"))
+install.packages(c("terra","sf","mapview","dismo","glmnet","maxnet","randomForest","rnaturalearth","dplyr","precrec"))
 library(terra)
 library(sf)
+library(dismo)
 
 # loading in my raster data
 meles<- read.csv("Melesmeles.csv")
+
+
+
