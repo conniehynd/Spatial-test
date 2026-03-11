bc8e0539de175d6c8a145d7c0f7fbd2bbc437d4c
conniehynd <constance.hyndman@postgrad.manchester.ac.uk>
2026-03-11 12:21:49 +0000

    setting up A1

--- A1.R
+++ A1.R
@@ -1,9 +1,10 @@
 setwd("~/Desktop/Spatial Ecology/Week_1")
 
 #Install packages
-install.packages(c("terra","sf","mapview","dismo","glmnet","maxnet","randomForest","rnaturalearth","dplyr","precrec"))
-library(terra)
-library(sf)
+install.packages(c("terra","sf","spatstat","cowplot","ggplot2","dismo","glmnet","maxnet","precrec","mlr","mapview","randomForest","rnaturalearth","dplyr"))
+library(terra) #for spatial data
+library(sf) #data frames with geometry
+library(spatstat) #for point process modelling and converting between raster and pixel image objects
 library(dismo)
 
 # loading in my raster data
